4fcc0420e6b4b8d56e54abf5ffca7e8bcdf77f9d
Jakub Kociucki <82371733+KociucKy@users.noreply.github.com>
2024-06-11 21:02:57 +0200

    Project init

---
+++ TabBarDemo/TabBarDemo/TabBarDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarDemoApp.swift
+//  TabBarDemo
+//
+//  Created by Kuba Kociucki on 11/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TabBarDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
